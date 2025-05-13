8d1ae76becf456929b8008f5ad7ace34183667ac
Morena_Mescia <morena.mescia98@gmail.com>
2025-05-13 16:30:26 +0200

    Add files via upload

---
+++ Martedì 13/EsempioPrimitivi.java
@@ -0,0 +1,10 @@
+public class EsempioPrimitivi {
+    public static void main(String[] args) {
+        float myFloat = 35e3f;
+        double myDouble = 12E4d;
+        // stampa i numeri primitivi
+
+        System.out.println("Valore di myFloat: " + myFloat);
+        System.out.println("Valore di myDouble: " + myDouble);
+    }
+}
