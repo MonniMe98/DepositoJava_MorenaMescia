9ff32cdc297639224d4c89b0f8bf3a887749b154
Morena_Mescia <morena.mescia98@gmail.com>
2025-05-13 16:40:32 +0200

    Add files via upload

---
+++ Martedì 13/EsempioStringa.java
@@ -0,0 +1,7 @@
+public class EsempioStringa {
+    public static void main(String[] args) {
+        String nome = "Morena";
+        System.out.println("Il nome è: " + nome); 
+        // Stampa un testo con il valore di nome
+    }
+}
